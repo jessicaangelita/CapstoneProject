e1be016d19a5966031db0e2e6a65e6f00dda29c8
Ahmad Syahbandi <saboonlbk@gmail.com>
2023-08-15 14:18:26 +0800

    perbaiki kesalahan classname

--- pages/homePage.js
+++ pages/homePage.js
@@ -5,10 +5,8 @@ export default function Home() {
   return (
     <>
       {/* <Sidebar/> */}
-      <div className="" flex flex-col w-full>
-        <HeaderHome />
-        <main className="p-8">Content</main>
-      </div>
+      <HeaderHome />
+      <main className="p-8">Content</main>
     </>
   );
 }
