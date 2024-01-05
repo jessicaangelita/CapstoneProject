40d745385bec221d11842227b9bf6ffa7ff057bc
Clex1911 <cleospike08@gmail.com>
2024-01-05 22:40:23 +0700

    Edit CSS Profile

--- frontend/components/EditProfile.js
+++ frontend/components/EditProfile.js
@@ -1,5 +1,99 @@
-// components/FormProfileEdit.js
+// // components/FormProfileEdit.js
 
+// import React, { useState } from "react";
+// import Head from "next/head";
+
+// const FormProfileEdit = ({ onSave }) => {
+//   const [fullName, setFullName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Simpan data profil yang diedit
+//     onSave({ fullName, email, password });
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8">
+//         <div>
+//             <Head>
+//                 <title>Edit Profile</title>
+//             </Head>
+//         </div>
+
+//       <div className="mb-4">
+//         <label htmlFor="fullName" className="block text-gray-700 text-sm font-bold mb-2">
+//           Full Name
+//         </label>
+//         {/* <input
+//           type="text"
+//           id="fullName"
+//           value={fullName}
+//           onChange={(e) => setFullName(e.target.value)}
+//           className="w-full border rounded py-2 px-3"
+//         /> */}
+//         <input
+//                   type="text"
+//                   name="first-name"
+//                   id="first-name"
+//                   placeholder='Name'
+//                   autoComplete="given-name"
+//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                 />
+//       </div>
+//       <div className="mb-4">
+//         <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+//           Email
+//         </label>
+//         {/* <input
+//           type="email"
+//           id="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           className="w-full border rounded py-2 px-3"
+//         /> */}
+//          <input
+//                   id="email"
+//                   name="email"
+//                   type="email"
+//                   placeholder='Email'
+//                   autoComplete="email"
+//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                 />
+//       </div>
+//       <div className="mb-4">
+//         <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+//           Password
+//         </label>
+//         {/* <input
+//           type="password"
+//           id="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           className="w-full border rounded py-2 px-3"
+//         /> */}
+//         <input
+//           className ="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//           // type={showPassword ? 'text' : 'password'}
+//           id='password'
+//           name='password'
+//           placeholder='Password'
+//           value={password}
+//         //  onChange={(e) => setPassword(e.target.value)}
+//           // required
+//         />
+//       </div>
+//       <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
+//         Save Changes
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default FormProfileEdit;
+
+// components/FormProfileEdit.js
 import React, { useState } from "react";
 import Head from "next/head";
 
@@ -10,80 +104,74 @@ const FormProfileEdit = ({ onSave }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Simpan data profil yang diedit
     onSave({ fullName, email, password });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8">
-        <div>
-            <Head>
-                <title>Edit Profile</title>
-            </Head>
-        </div>
-      <div className="mb-4">
-        <label htmlFor="fullName" className="block text-gray-700 text-sm font-bold mb-2">
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8 flex flex-col items-center">
+      <div>
+        <Head>
+          <title>Edit Profile</title>
+        </Head>
+      </div>
+
+      {/* Fullname */}
+      {/* <div className="mt-0.2 mb-4 flex flex-col items-center"> */}
+      <div className="mb-[3%]">
+        <label htmlFor="fullName" className="block text-black-700 text-sm font-bold mb-2">
           Full Name
         </label>
-        {/* <input
-          type="text"
-          id="fullName"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-          className="w-full border rounded py-2 px-3"
-        /> */}
-        <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  placeholder='Name'
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+        <div className="w-full bg-white flex items-center border-gray-300 border rounded-lg px-3 py-1 focus:outline-none shadow shadow-black">
+          <input
+            type="text"
+            name="first-name"
+            id="first-name"
+            placeholder='Name'
+            autoComplete="given-name"
+            className="pl-2 py-1 w-72 focus:outline-none"
+          />
+        </div>
       </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+
+      {/* Email */}
+      {/* <div className="mb-4 flex flex-col items-center"> */}
+      <div className="mb-[3%]">
+        <label htmlFor="email" className="block text-black-700 text-sm font-bold mb-2">
           Email
         </label>
-        {/* <input
-          type="email"
+        <div className="w-full bg-white flex items-center mb-[3%] border-gray-300 border rounded-lg px-3 py-1 focus:outline-none shadow shadow-black">
+        <input
           id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full border rounded py-2 px-3"
-        /> */}
-         <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder='Email'
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+          name="email"
+          type="email"
+          placeholder='Email'
+          autoComplete="email"
+          className="pl-2 py-1 w-72 focus:outline-none"
+        />
+       </div>
       </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+
+      {/* Password */}
+      {/* <div className="mb-4 flex flex-col items-center"> */}
+      <div className="mb-[3%]">
+        <label htmlFor="password" className="block text-black-700 text-sm font-bold mb-2">
           Password
         </label>
-        {/* <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full border rounded py-2 px-3"
-        /> */}
+        <div className="w-full bg-white flex items-center mb-[3%] border-gray-300 border rounded-lg px-3 py-1 focus:outline-none shadow shadow-black">
         <input
-          className ="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          // type={showPassword ? 'text' : 'password'}
+          className="pl-2 py-1 w-72 focus:outline-none"
           id='password'
           name='password'
           placeholder='Password'
           value={password}
-        //  onChange={(e) => setPassword(e.target.value)}
-          // required
         />
+        </div>
       </div>
-      <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
+
+
+      <button 
+      type="submit" 
+      className="mx-auto bg-blue-500 text-white py-2 px-4 rounded justify-center text-center items-center">
         Save Changes
       </button>
     </form>
--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,10 +1,147 @@
+// // pages/profile.js
+
+// import React, { useState, useEffect, useRouter } from "react";
+// import HeaderHome from "../components/headerhome"
+// import FormProfileEdit from "../components/EditProfile";
+// import axios from "../pages/api/axios";
+// import Head from "next/head";
+
+// const Profile = () => {
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [profileData, setProfileData] = useState({
+//     fullname: '',
+//     uname: '',
+//     email: '',
+//     pass: ''
+//   });
+
+//   const handleSaveProfile = (editedData) => {
+//     // Save edited data to the backend or application state
+//     setProfileData(editedData);
+//     setIsEditing(false);
+//   };
+
+//   // const router = useRouter();
+//   // const { userid } = router.query;
+
+//   useEffect(() => {
+//     // Fetch profile data from the API
+//     const fetchProfileData = async () => {
+//       try {
+//         const response = await axios.get(`http://localhost:8050/project/user/connected/${userid}`);
+//         const data = await response.data;
+
+//         // Assuming the API response structure is { fullName, email, password }
+//         setProfileData(data);
+//       } catch (error) {
+//         console.error("Error fetching profile data:", error);
+//       }
+//     };
+
+//     fetchProfileData();
+//   }, []); // The empty dependency array ensures the effect runs only once on component mount
+
+//   return (
+//     <div>
+//       <div>
+//             <Head>
+//                 <title>Profile</title>
+//             </Head>
+//         </div>
+//       <HeaderHome />
+      
+//       <div className="container mx-auto mt-24 ml-36 p-4">
+//         {/* <h1 className="text-2xl font-bold mb-4 mr-28">Profile</h1> */}
+//         <div className="flex justify-center items-center">
+
+//         {/* Information */}
+//         <div className="bg-white flex flex-col items-center max-w-screen-lg border-8 border-gradient-to-l overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,1)] w-2/3 md:flex-row text-black">
+//           {/* Profile display */}
+//           <div className= "p-6 rounded-2xl m-1 flex flex-col shadow-md justify-start items-center w-1/3 from-primary-mediumblue via-white to-primary-mediumblue bg-gradient-to-tr h-96">
+//             <img className="inline-block h-24 w-24 rounded-full ring-2 ring-white"
+//                   src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+//                   alt=""
+//               />
+//               <h1 className="mb-2 mt-5 justify-start font-bold text-2xl text-black">
+//                 <strong>{profileData.fullname}AHMAD</strong> 
+//               </h1>
+//               <p className="font-semibold text-xl">
+//                 Web Developer
+//               </p>
+//           </div>
+
+          
+//           <div className="flex justify-center items-center space-y-4 flex-col">
+//             <div className="flex flex-col items-center mb-4 mr-12">
+//               <h1 className="text-slate-700 text-4xl font-semibold">Profile</h1>
+//             </div>
+
+
+//           {isEditing ? (
+//             <FormProfileEdit onSave={handleSaveProfile} />
+//           ) : (
+//             <div className="mt-6 pl-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+//             <div className="sm:col-span-3">
+//               <label htmlFor="first-name" className="block text-3xl font-medium leading-6 text-gray-900">
+//                 Fullname
+//               </label>
+//               <div className="mt-2 text-xl">
+//               <strong>Belinda</strong> {profileData.username}
+//               </div>
+//             </div>
+
+//             <div className="sm:col-span-3">
+//               <label htmlFor="last-name" className="md:flex-row block text-3xl font-medium leading-6 text-gray-900">
+//                 Username
+//               </label>
+//               <div className="mt-2 text-xl">
+//               <strong>Clex23</strong> {profileData.username}
+//               </div>
+//             </div>
+
+//             <div class="sm:col-span-3">
+//               <label for="email" class="block text-3xl font-medium leading-6 text-gray-900">
+//                 Email address</label>
+//               <div class="mt-2">
+//               <strong>jahbahbs@gmail.com</strong> {profileData.pass}
+//               </div>
+//             </div>
+
+//             <div class="sm:col-span-4">
+//               <label for="email" class="block text-3xl font-medium leading-6 text-gray-900">
+//                 Password</label>
+//               <div class="mt-2">
+//               <strong>Password</strong> {profileData.pass}
+//               </div>
+//             </div>
+
+//               {/* Button Edit */}
+//               <div className="mt-6 flex items-center justify-end">
+//                 <button
+//                   onClick={() => setIsEditing(true)}
+//                   className="bg-blue-500 text-white text-lg py-3 px-7 rounded mt-9"
+//                 >
+//                   Edit
+//                 </button>
+//               </div>
+//             </div>
+//           )}
+//           </div>
+//           </div>
+//           </div>
+//         </div>
+//     // </div>
+//   );
+// };
+
+// export default Profile;
+
 // pages/profile.js
 
 import React, { useState, useEffect, useRouter } from "react";
-import HeaderHome from "../components/headerhome"
+import HeaderHome from "../components/Headerhome"
 import FormProfileEdit from "../components/EditProfile";
 import axios from "../pages/api/axios";
-// import user from "../public/assets/user.png"
 import Head from "next/head";
 
 const Profile = () => {
@@ -43,40 +180,24 @@ const Profile = () => {
   }, []); // The empty dependency array ensures the effect runs only once on component mount
 
   return (
-    <div>
+    <div className="min-h-screen bg-primary-darkblue flex flex-col w-full">
       <div>
             <Head>
                 <title>Profile</title>
             </Head>
         </div>
-      <HeaderHome />
-      
-      <div className="container mx-auto mt-24 ml-36 p-4">
-        {/* <h1 className="text-2xl font-bold mb-4 mr-28">Profile</h1> */}
-        <div className="flex justify-center items-center">
+      <HeaderHome />  
+      <div className="flex justify-center items-center mt-12 ml-40">
 
         {/* Information */}
-        <div className="bg-white flex flex-col items-center max-w-screen-lg border-8 border-gradient-to-l overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,1)] w-2/3 md:flex-row text-black">
+        <div className="bg-white flex flex-col items-center max-w-screen-lg border-8 border-gradient-to-l overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,1)] w-1/2 md:flex-row text-black">
           {/* Profile display */}
-          <div className= "p-6 rounded-2xl m-1 flex flex-col shadow-md justify-start items-center w-1/3 from-primary-mediumblue via-white to-primary-mediumblue bg-gradient-to-tr h-96">
-            <img className="inline-block h-24 w-24 rounded-full ring-2 ring-white"
-                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-              />
-              <h1 className="mb-2 mt-5 justify-start font-bold text-2xl text-black">
-                <strong>{profileData.fullname}AHMAD</strong> 
-              </h1>
-              <p className="font-semibold text-xl">
-                Web Developer
-              </p>
-          </div>
+          <div className= "p-2 rounded-2xl m-1 flex flex-col shadow-md justify-start items-center w-full from-primary-mediumblue via-white to-primary-mediumblue bg-gradient-to-tr h-full">
 
-          
           <div className="flex justify-center items-center space-y-4 flex-col">
-            <div className="flex flex-col items-center mb-4 mr-12">
-              <h1 className="text-slate-700 text-4xl font-semibold">Profile</h1>
+            <div className="flex flex-col items-center mb-4">
+              <h1 className="text-black-700 text-4xl font-semibold">Profile</h1>
             </div>
-
 
           {isEditing ? (
             <FormProfileEdit onSave={handleSaveProfile} />
@@ -86,7 +207,7 @@ const Profile = () => {
               <label htmlFor="first-name" className="block text-3xl font-medium leading-6 text-gray-900">
                 Fullname
               </label>
-              <div className="mt-2 text-xl">
+              <div className="mt-2 text-lg">
               <strong>Belinda</strong> {profileData.username}
               </div>
             </div>
@@ -95,7 +216,7 @@ const Profile = () => {
               <label htmlFor="last-name" className="md:flex-row block text-3xl font-medium leading-6 text-gray-900">
                 Username
               </label>
-              <div className="mt-2 text-xl">
+              <div className="mt-2 text-lg">
               <strong>Clex23</strong> {profileData.username}
               </div>
             </div>
@@ -103,7 +224,7 @@ const Profile = () => {
             <div class="sm:col-span-3">
               <label for="email" class="block text-3xl font-medium leading-6 text-gray-900">
                 Email address</label>
-              <div class="mt-2">
+              <div class="mt-2 text-lg">
               <strong>jahbahbs@gmail.com</strong> {profileData.pass}
               </div>
             </div>
@@ -111,7 +232,7 @@ const Profile = () => {
             <div class="sm:col-span-4">
               <label for="email" class="block text-3xl font-medium leading-6 text-gray-900">
                 Password</label>
-              <div class="mt-2">
+              <div class="mt-2 text-lg">
               <strong>Password</strong> {profileData.pass}
               </div>
             </div>
@@ -120,19 +241,20 @@ const Profile = () => {
               <div className="mt-6 flex items-center justify-end">
                 <button
                   onClick={() => setIsEditing(true)}
-                  className="bg-blue-500 text-white text-lg py-3 px-7 rounded mt-9"
+                  className="bg-blue-500 text-white text-lg py-3 px-5 rounded mt-9"
                 >
                   Edit
                 </button>
               </div>
             </div>
           )}
-          </div>
-          </div>
+                </div>
+            </div>
+           </div>
           </div>
         </div>
-    // </div>
   );
 };
 
 export default Profile;
+
