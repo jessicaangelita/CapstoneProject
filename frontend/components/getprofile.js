a59b0fa1e172e0a0c578d9f130ae0d8bfd9de67d
Clex1911 <cleospike08@gmail.com>
2024-01-17 21:23:00 +0700

    fix get profile

--- frontend/components/getprofile.js
+++ frontend/components/getprofile.js
@@ -4,6 +4,8 @@ import FormProfileEdit from "../components/EditProfile";
 import axios from "../pages/api/axios";
 import Head from "next/head";
 import HeaderHome from "../components/headerhome"
+import { FaPencilAlt} from "react-icons/fa";
+import { RiLockPasswordFill } from 'react-icons/ri';
 
 export default function ContentProfile () {
 
@@ -19,10 +21,45 @@ export default function ContentProfile () {
     email: '',
   });
 
-  const handleSaveProfile = (editedData) => {
-    setProfileData(editedData);
-    setIsEditing(false);
+  const router = useRouter();
+
+  const handleEditButtonClick = () => {
+    router.push("/edit"); // Sesuaikan dengan path yang sesuai untuk halaman edit profil Anda
   };
+  
+//   const handleEdit = (item) => {
+//     setData(item);
+// };
+
+  // const handleSaveProfile = async (editedData) => {
+  //  try {
+  //     const response = await axios.put(
+  //       "http://localhost:8050/user/profile",
+  //       editedData,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  //         },
+  //       }
+  //     );
+  
+  //     // Pastikan permintaan berhasil
+  //     if (response.status === 200) {
+  //       console.log("Profile updated successfully!");
+  //       setIsEditing(false); 
+  //       fetchProfileData(); 
+
+  //       router.push("/profile");
+  //       console.log(router);
+  //     } else {
+  //       console.error("Failed to update profile:", response.data);
+        
+  //     }
+  //   } catch (error) {
+  //     console.error("Error updating profile:", error);
+      
+  //   }
+  // };
     
     // Fetch profile data from the API
     const fetchProfileData = async () => {
@@ -54,50 +91,139 @@ export default function ContentProfile () {
         fetchProfileData();
       }, []);
 
+//   return (
+//     <div className="min-h-screen bg-primary-darkgrey flex flex-col w-full">
+//       <div>
+//             <Head>
+//                 <title>Profile</title>
+//             </Head>
+//         </div>
+//       <HeaderHome />  
+//       <div className="flex justify-center items-center mt-5 ml-40">
+
+//         {/* Information */}
+//         <div className="bg-white flex flex-col items-center max-w-screen-lg border-8 border-gradient-to-l overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,1)] w-1/2 md:flex-row text-black">
+//           {/* Profile display */}
+//           <div className= "p-2 rounded-2xl m-1 flex flex-col shadow-md justify-start items-center w-full from-primary-mediumblue via-white to-primary-mediumblue bg-gradient-to-tr h-full">
+
+//           <div className="flex justify-center items-center space-y-4 flex-col">
+//             <div className="flex flex-col items-center mb-4">
+//               <h1 className="text-black-700 text-4xl font-semibold">Profile</h1>
+//             </div>
+
+//           {isEditing ? (
+//             <FormProfileEdit onSave={handleSaveProfile} profileData={profileData} />
+//           ) : (
+//             <div className="mt-6 pl-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+//             <div className="sm:col-span-3">
+//               <label htmlFor="first-name" className="block text-3xl font-medium leading-6 text-gray-900">
+//                 Fullname
+//               </label>
+//               <div className="mt-2 text-lg">
+//               <strong>{data.data?.name}</strong> 
+//               </div>
+//             </div>
+
+//             <div className="sm:col-span-3">
+//               <label htmlFor="last-name" className="md:flex-row block text-3xl font-medium leading-6 text-gray-900">
+//                 Username
+//               </label>
+//               <div className="mt-2 text-lg">
+//               <strong>{data.data?.username}</strong>
+//               </div>
+//             </div>
+
+//             <div class="sm:col-span-3">
+//               <label for="email" class="block text-3xl font-medium leading-6 text-gray-900">
+//                 Email address</label>
+//               <div class="mt-2 text-lg">
+//               <strong>{data.data?.email}</strong> 
+//               </div>
+//             </div>
+
+//               {/* Button Edit */}
+//               <div className="mt-6 flex items-center justify-end">
+//                 <button
+//                   onClick={() => setIsEditing(true)}
+//                   className="bg-blue-500 text-white text-lg py-3 px-5 rounded mt-2 ml-5"
+//                 >
+//                   Edit
+//                 </button>
+//               </div>
+
+//               <RiLockPasswordFill />
+
+//             </div>
+//           )}
+//                 </div>
+//             </div>
+//            </div>
+//           </div>
+//         </div>
+//   );
+
+// };
+
   return (
-    <div className="min-h-screen bg-primary-darkgrey flex flex-col w-full">
+    <div className="min-h-screen bg-primary-darkgrey flex flex-col w-full"> 
       <div>
             <Head>
                 <title>Profile</title>
             </Head>
         </div>
-      <HeaderHome />  
-      <div className="flex justify-center items-center mt-5 ml-40">
+      <HeaderHome />
+      
+      <div className="flex justify-center items-center mt-12 ml-40">
+        {/* <h1 className="text-2xl font-bold mb-4 mr-28">Profile</h1> */}
+        <div className="flex justify-center items-center">
 
         {/* Information */}
-        <div className="bg-white flex flex-col items-center max-w-screen-lg border-8 border-gradient-to-l overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,1)] w-1/2 md:flex-row text-black">
+        <div className="bg-white flex flex-col items-center max-w-screen-lg border-8 border-gradient-to-l overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,1)] w-2/3 md:flex-row text-black">
           {/* Profile display */}
-          <div className= "p-2 rounded-2xl m-1 flex flex-col shadow-md justify-start items-center w-full from-primary-mediumblue via-white to-primary-mediumblue bg-gradient-to-tr h-full">
+          <div className= "p-6 flex flex-col shadow-md justify-start items-center py-10 w-2/3 from-primary-mediumblue via-white to-primary-mediumblue bg-gradient-to-tr">
+            <img className=" md:flex-row inline-block h-50 w-56 rounded ring-2 ring-white"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+              />
+              {/* <h1 className="mb-2 mt-5 justify-start font-bold text-2xl text-black">
+                <strong>{profileData.name}AHMAD</strong> 
+              </h1> */}
+              {/* <p className="font-semibold text-xl">
+                Web Developer
+              </p> */}
+          </div>
 
+          
           <div className="flex justify-center items-center space-y-4 flex-col">
-            <div className="flex flex-col items-center mb-4">
-              <h1 className="text-black-700 text-4xl font-semibold">Profile</h1>
+            <div className="flex flex-col items-center mt-2 mb-4 mr-10">
+              <h1 className="text-slate-700 text-3xl font-semibold">Profile</h1>
             </div>
 
-          {isEditing ? (
+
+          {/* {isEditing ? (
             <FormProfileEdit onSave={handleSaveProfile} />
-          ) : (
+          ) : ( */}
             <div className="mt-6 pl-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-3xl font-medium leading-6 text-gray-900">
+              <label htmlFor="first-name" className="block text-2xl font-medium leading-6 text-gray-900">
                 Fullname
               </label>
               <div className="mt-2 text-lg">
-              <strong>{data.data?.name}</strong> 
+              <strong>{data.data?.name}</strong>
               </div>
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="md:flex-row block text-3xl font-medium leading-6 text-gray-900">
+              <label htmlFor="last-name" className="md:flex-row block text-2xl font-medium leading-6 text-gray-900">
                 Username
               </label>
               <div className="mt-2 text-lg">
-              <strong>{data.data?.username}</strong>
+              <strong>{data.data?.username}</strong> 
               </div>
             </div>
 
-            <div class="sm:col-span-3">
-              <label for="email" class="block text-3xl font-medium leading-6 text-gray-900">
+            <div class="sm:col-span-4">
+              <label for="email" class="block text-2xl font-medium leading-6 text-gray-900">
                 Email address</label>
               <div class="mt-2 text-lg">
               <strong>{data.data?.email}</strong> 
@@ -105,56 +231,23 @@ export default function ContentProfile () {
             </div>
 
               {/* Button Edit */}
-              <div className="mt-6 flex items-center justify-end">
+              <div className="mt-10 flex items-center justify-end">
                 <button
-                  onClick={() => setIsEditing(true)}
-                  className="bg-blue-500 text-white text-lg py-3 px-5 rounded mt-2 ml-5"
+                  // onClick={() => setIsEditing(true)}
+                  onClick={handleEditButtonClick}
+                  className="bg-blue-500 text-white text-lg py-3 px-7 rounded mt-9 mb-5"
                 >
                   Edit
                 </button>
               </div>
             </div>
-          )}
-                </div>
-            </div>
-           </div>
+          {/* )} */}
+          </div>
+          </div>
           </div>
         </div>
+    </div>
   );
-
 };
 
-export async function getStaticPaths() {
-    // Mendapatkan ID dari local storage
-    const accessToken = localStorage.getItem("accessToken");
-    const decodedToken = decodeToken(accessToken); // Anda perlu membuat fungsi decodeToken sesuai kebutuhan
-  
-    // Membuat path berdasarkan ID dari access token
-    const paths = [
-      { params: { id: decodedToken.userId } },
-      // ... tambahkan path lainnya sesuai kebutuhan
-    ];
-  
-    return { paths, fallback: false };
-  }
-  
-  export async function getStaticProps({ params }) {
-    // Mendapatkan data user berdasarkan ID
-    const response = await axios.get(
-      `http://localhost:8050/user/profile/${params.id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      }
-    );
-  
-    const dataFetch = await response.data;
-  
-    return {
-      props: {
-        data: dataFetch,
-      },
-    };
-  }
   
